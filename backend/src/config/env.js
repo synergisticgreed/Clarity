7eb49cf2b2c77a272a6f48e3f3da233e55994074
synergisticgreed <sameergptnsr@gmail.com>
2025-10-12 23:06:20 +0530

    env setup

---
+++ backend/src/config/env.js
@@ -0,0 +1,6 @@
+import "dotenv/config";
+
+const ENV={
+    PORT: process.env.PORT || 5000,
+}
+export default ENV;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,13 @@
 import express from "express"
 import cors from "cors"
+import ENV from "./config/env.js"
 
 const app=express();
 
 app.use(express.json());
 app.use(cors());
 
-const PORT=5000;
+const PORT=ENV.PORT;
 
 app.get("/",(req,res)=>{
     res.send("Hello from backend");
