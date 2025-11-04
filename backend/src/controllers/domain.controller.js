c832f981bb9446a181be065f485d2628e57bc17e
HNikki0303 <nikitapant496@gmail.com>
2025-11-04 22:49:13 +0530

    add createDomain function to handle domain creation

---
+++ backend/src/controllers/domain.controller.js
@@ -0,0 +1,26 @@
+import {Domain} from "./models/domain.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { APIError } from "../utils/APIError.js";
+import { APIResponse } from "../utils/APIRespnse.js";
+
+// just to create a category 
+const createDomain = asyncHandler(async(req,res)=>{
+    const {domainName} = req.body;
+    if(domainName===""){
+        throw new APIError(404," please give the domain name");
+    }
+    const createdDomain = await Domain.create({
+        domainName
+    })
+    return res
+    .status(200)
+    .json(
+        new APIResponse(
+            200,
+            createdDomain,
+            "Domain created"
+        )
+    )
+})
+
+export {createDomain}
